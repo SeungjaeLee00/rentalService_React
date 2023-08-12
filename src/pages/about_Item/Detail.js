5c14185bf0bc2cbf7795b07740accb0acc28e4f0
leejaejae <leesengjie@naver.com>
2023-08-12 21:54:30 +0900

    pagination 예시 구현

--- src/pages/about_Item/Detail.js
+++ src/pages/about_Item/Detail.js
@@ -3,42 +3,84 @@ import { NavLink, useNavigate } from 'react-router-dom';
 import { useInView } from 'react-intersection-observer';  // 무한 스크롤용 라이브러리
 
 import '../../App.css';
-import CommentInput from '../about_Item/commentInput';
+import HorizonLine from '../HorizonLine';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Detail () {
 
     let navigate = useNavigate();  
-    const [comments, setComments] = useState([
-        { id: 1, name: 'Minjoo Park', content: 'I like it!', }
-      ]);
+    // const [comments, setComments] = useState([
+    //     { id: 1, content: 'I like it!' }
+    //   ]);
     
-      const nextId = useRef(1);
+    // const nextId = useRef(1);
     
-      const onInsert = useCallback(
-        (name, content) => {
-          const comment = {
-            id: nextId.current,
-            name,
-            content
-          };
-          console.log(name);
-          console.log(content);
-          setComments(comments => comments.concat(comment));
-          nextId.current += 1; //nextId 1씩 더하기
-        },
-        [comments],
-      );
+    // const onInsert = useCallback(
+    //     (content) => {
+    //       const comment = {
+    //         id: nextId.current,
+    //         content
+    //       };
+    //       console.log(content);
+    //       setComments(comments => comments.concat(comment));
+    //       nextId.current += 1; 
+    //     },
+    //     [comments],
+    //   );
+
+    // const [value, setValue] = useState({
+    //     content: ''
+    // });
+
+    // const onChangeContent = useCallback(
+    //     (e) => {
+    //         setValue({
+    //             content: e.target.value,
+    //         });
+    //     },
+    //     [value]
+    // );
+
+    // const onSubmit = useCallback(
+    //     e => {
+    //         onInsert(value.content);
+    //         setValue({
+    //             content: ''
+    //         });
+
+    //         e.preventDefault();
+    //     },
+    //     [onInsert, value],
+    // );
 
     return(
         <div className='App'>
-            <h1>상품 디테일</h1>
-            <CommentInput onInsert={onInsert} />
+
+        {/* 여기다가 나머지 구현하면 댐 */}
+
+        {/* <HorizonLine />
+
+            <div style={{ padding: "20px" }}>
+                <h3>댓글</h3>
+                <form className="CommentInsert" onSubmit={onSubmit}
+                    style={{ display:'flex' }}>
+                    <textarea placeholder="    댓글을 입력하세요."
+                        cols="90"
+                        rows="2"
+                        style={{ background_color: "transparent", 
+                        border: "2px solid rgba(0, 0, 0, 0.23)",
+                        borderRadius: "7px", marginLeft: "270px" }}
+                        value={value.content}
+                        onChange={onChangeContent}
+                    />
+                    <button type="submit" style={{ border:'none', marginLeft: "5px" }}>보내기</button>
+                </form>
+            </div>
 
             <div className='upload_item' style={{position:"fixed", right: '45px', bottom: '30px'}}>
                 <button style={{ borderRadius:"30px", fontSize:'20px',  width:"100px", height: "50px", border:"none" }}
                         onClick={() => navigate('/itemmain/upload-item')}> + 글쓰기 </button>
-            </div> 
+            </div>  */}
         </div>
     )
 }
