f6a659aede25d7fe175c2013081e4720930b17cd
choims <chlaudtn0257@gmail.com>
2023-09-11 18:55:08 +0900

    마이페이지 내게시물조회 조회,클릭완성

--- src/pages/my_page/MyPost.js
+++ src/pages/my_page/MyPost.js
@@ -1,21 +1,23 @@
 import axios from "axios";
 import { useEffect } from "react";
 import { useState } from "react";
-import { useOutletContext } from "react-router-dom";
+import { Navigate, useNavigate, useOutletContext } from "react-router-dom";
 
 
 export default function MyPost(props)
 {
     const {mypost, setMyPost} = useOutletContext();
+    const navigate = useNavigate();
     console.log(mypost);    
+
     return(
         <div className="MyPost-wrap">
             { mypost ? <div> <div className="post-top">
                 <p style={{fontSize:"25px"}}>게시물내역 조회</p>
             </div>
-            <hr></hr>
+            
             <div className="post-bottom">  
-                <table>
+                <table style={{width:"1500px", marginTop:"30px"} }>
                   <thead>
                     <tr>
                         <th >게시물 정보</th>
@@ -25,9 +27,9 @@ export default function MyPost(props)
                   </thead>
                   <tbody>
                   {mypost.postList.map(a=>(
-                            <tr>
-                                <td>{a.title}</td>
-                                <td>{a.createdTime}</td>
+                            <tr onClick={()=>navigate("/itemmain/detail/"+a.id)}>
+                                <td style={{fontSize:"20px" , fontWeight:"bold"}} >{a.title}</td>
+                                <td>{a.createdTime.replace("T"," ")}</td>
                                 <td>{"대여가능"}</td>
                             </tr>
                         ))}
