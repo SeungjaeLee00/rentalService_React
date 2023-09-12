46b55240123223a7bf4dff436c4c11fa61904252
choims <chlaudtn0257@gmail.com>
2023-09-12 16:45:19 +0900

    mypage 게시물 img추가, 좋아요, 게시물 hover효과추가

--- src/pages/my_page/MyPost.js
+++ src/pages/my_page/MyPost.js
@@ -4,42 +4,49 @@ import { useState } from "react";
 import { Navigate, useNavigate, useOutletContext } from "react-router-dom";
 
 
-export default function MyPost(props)
-{
-    const {mypost, setMyPost} = useOutletContext();
+export default function MyPost(props) {
+    const { mypost, setMyPost } = useOutletContext();
     const navigate = useNavigate();
-    console.log(mypost);    
+    console.log(mypost);
 
-    return(
+    return (
         <div className="MyPost-wrap">
-            { mypost ? <div> <div className="post-top">
-                <p style={{padding:"10px" ,fontSize:"25px"}}>게시물내역 조회</p>
+            {mypost ? <div> <div className="post-top">
+                <p style={{ padding: "10px", fontSize: "25px" }}>게시물내역 조회</p>
             </div>
-            
-            <div className="post-bottom">  
-                <table style={{width:"1500px", marginTop:"20px"} }>
-                  <thead>
-                    <tr>
-                        <th >게시물 정보</th>
-                        <th >게시물 작성일자</th>
-                        <th >게시물 대여상태</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                  {mypost.postList.map(a=>(
-                            <tr onClick={()=>navigate("/itemmain/detail/"+a.id)}>
-                                <td style={{fontSize:"20px" , fontWeight:"bold"}} >{a.title}</td>
-                                <td>{a.createdTime.replace("T"," ")}</td>
-                                <td>{"대여가능"}</td>
+
+                <div className="post-bottom">
+                    <table style={{ width: "1500px", marginTop: "20px" }}>
+                        <thead>
+                            <tr>
+                                <th >게시물 정보</th>
+                                <th >게시물 작성일자</th>
+                                <th >게시물 대여상태</th>
                             </tr>
-                        ))}
-                    
-                  </tbody>
-                </table> 
-                
-            </div> </div>: null}
-            
-            
+                        </thead>
+                        <tbody>
+                            {mypost.postList.map(a => (
+                                <tr onClick={() => navigate("/itemmain/detail/" + a.id)}>
+                                    <td className="first-td">
+                                        <div>
+                                            <img style={{ width: "100px", height: "100px" }} src={'https://sharingplatformbucket.s3.ap-northeast-2.amazonaws.com/post/' + a.link}></img>
+                                        </div>
+                                        <div className="first-td-info">
+                                            <div style={{ fontSize: "20px", fontWeight: "bold", marginTop:"30px" }}>{a.title}</div>
+                                        </div>
+
+                                    </td>
+                                    <td>{a.createdTime.replace("T", " ")}</td>
+                                    <td>{"대여가능"}</td>
+                                </tr>
+                            ))}
+
+                        </tbody>
+                    </table>
+
+                </div> </div> : null}
+
+
         </div>
     )
 }
