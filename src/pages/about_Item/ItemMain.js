47f714b7cdfa9647b27cbf25c05bf94de926cd9a
choims <chlaudtn0257@gmail.com>
2023-09-19 15:02:08 +0900

    09.19 push

--- src/pages/about_Item/ItemMain.js
+++ src/pages/about_Item/ItemMain.js
@@ -47,6 +47,7 @@ function ItemMain() {
     currentPosts = store.slice(indexOfFirst, indexOfLast);
     return currentPosts;
   };
+  
   if(loading) return <div>로딩중..</div>;
   if(error) return <div>에러가 발생했습니다</div>;
 
