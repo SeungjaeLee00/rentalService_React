8f95d4fa9c52ce26cdb64ee11dc213b137ca9e84
choims <chlaudtn0257@gmail.com>
2023-10-09 19:12:45 +0900

    회원가입(Signup.js) css, 사용자 입력데이터 수정

--- src/pages/SignUp/Signup.js
+++ src/pages/SignUp/Signup.js
@@ -12,37 +12,65 @@ function Signup() {
     //email -> useRef로 설정. useState로 하면 불필요한 리렌더링 발생 , 
     //실사간 유효성 검사 x -> 인증하기 버튼을 클릭하기 때문.
     const email = useRef();
+    //비밀번호 확인 
+    const verifypw = useRef();
+    //이메일인증 확인 -> 인증메일 보내면 -> 확인문구 출력 
     const [emailcheck, setEmailCheck] = useState(false);
+    //우편번호 표시 state 
     const [zipcode, setZipCode] = useState();
+    //주소 오리지널 값 state 
     const [originaddress, setOriginAddress] = useState();
+    //db에 넣기위한 형식 변경된 주소 값 
     const [address, setAddress] = useState();
-
-    const [password, setPassword] = useState();
-    const [repassword, setRePassword] = useState();
+    //주소모달 open state 
     const [addressisopen, setAddressIsOpen] = useState(false);
 
     const onSubmit = (data) => {
-        //이메일 따로추가. ref와 register 같이 사용 x 
-        data.username = email.current.value;
-        //address 따로추가. 지도 라이브러리사용.
-        let address = {
-            city: originaddress[1],
-            district: originaddress[2],
-            street: originaddress[3],
-            zipCode: originaddress[4]
+        console.log(data.password);
+        console.log(verifypw);
+        //주소가 선택되지 않았으면
+        if (!originaddress) {
+            alert('주소를 선택해주세요');
+            addresstoggle();
         }
-        data.address = address;
-    
-        if (password != repassword) {
-            alert('비밀번호를 확인해주세요');
+        else {
+            //이메일 따로추가. ref와 register 같이 사용 x 
+            data.username = email.current.value;
+            //address 따로추가. 지도 라이브러리사용.
+            let address = {
+                city: originaddress[1],
+                district: originaddress[2],
+                street: originaddress[3],
+                zipCode: originaddress[4]
+            }
+            //data에 address 추가 
+            data.address = address;
+            if (data.password != verifypw.current.value) {
+                alert('비밀번호가 일치하지 않습니다');
+                verifypw.current.focus();
+            }
+            else {
+                console.log(data);
+                axios.post('/auth/sign-up', data)
+                    .then(response => {
+                        console.log(response);
+                    }).catch(error => {
+                        if (error.response.data.result.msg == "해당 이메일로 가입된 계정이 이미 존재합니다.") {
+                            alert('해당 이메일로 가입된 계정이 이미 존재합니다.')
+                        }
+                        else if (error.response.data.result.msg == "인증번호가 일치하지 않습니다.") {
+                            alert("인증번호가 일치하지 않습니다.")
+                        }
+                        else if (error.response.data.result.msg == "해당 닉네임은 이미 사용중입니다.") {
+                            alert("해당 닉네임은 이미 사용중입니다.")
+                        }
+                        console.log(error);
+                    })
+            }
         }
-      
-        axios.post('/auth/sign-up', data)
-            .then(response => {
-                console.log(response);
-            }).catch(error => {
-                console.log(error);
-            })
+
+
+
     }
 
     const { register, handleSubmit, formState: { errors }, } = useForm();
@@ -59,16 +87,15 @@ function Signup() {
 
                 })
                 .catch(error => {
-                    if(error.response.data.code=='409')
-                    {
+                    if (error.response.data.code == '409') {
                         alert('이미 이메일을 전송하였습니다.');
                     }
-                    console.error('이메일 전송 실패:', error.response.data.result);
+                    console.log(error);
                 });
         }
         //이메일을 입력하지 않은 경우.
         else {
-            alert('이메일을 확인해주세요');
+            alert('이메일을 입력해주세요');
             email.current.focus();
         }
 
@@ -87,10 +114,10 @@ function Signup() {
         setAddress(temp);
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         //페이지 로드시 이메일 input에 자동으로 focus 
         email.current.focus();
-    },[])
+    }, [])
 
     return (
         <div className='signupwrap'>
@@ -101,87 +128,95 @@ function Signup() {
                 <form onSubmit={handleSubmit(onSubmit)}>
                     <div className='email-wrap'>
                         <div>
-                        <label>이메일</label>
-                        <input className="inputField"
-                            ref={email}
-                            placeholder="abcdef@google.com" />
-                        <button className='emailbtn' onClick={(e) => {
-                            //새로고침방지
-                            e.preventDefault();
-                            SendEmail();
-                        }}> 인증 </button>
+                            <label>이메일</label>
+                            <input className="inputField"
+                                ref={email}
+                                placeholder="abcdef@google.com" />
+                            <button className='emailbtn' onClick={(e) => {
+                                //새로고침방지
+                                e.preventDefault();
+                                SendEmail();
+                            }}> 인증 </button>
                         </div>
-                        {emailcheck ? <h5 style={{color:"black"}}>이메일을 전송하였습니다</h5> : null}
+                        {emailcheck ? <h5 style={{ color: "black" }}>이메일을 전송하였습니다</h5> : null}
                     </div>
 
                     <div className='key-wrap'>
-                        <label>이메일 인증키</label>
-                        <input className="inputField"
-                            placeholder="인증번호를 입력하세요" {...register('authKey', {
-                                required: '인증번호를 입력해주세요',
-                            })} />
+                        <div>
+                            <label>이메일 인증키</label>
+                            <input className="inputField"
+                                placeholder="인증번호를 입력하세요" {...register('authKey', {
+                                    required: '인증번호를 입력해주세요',
+                                })} />
+                        </div>
                         {errors.authKey && <p>{errors.authKey.message}</p>}
                     </div>
 
                     <div className='nickname-wrap'>
-                        <label>닉네임</label>
-                        <input className="inputField"
-                            placeholder="최소 2자~ 최대15자" {...register('nickname', {
-                                required: '닉네임을 입력해주세요',
-                                minLength: {
-                                    value: 2,
-                                    message: '최소 2자 이상을 입력해주세요'
-                                },
-                                maxLength: {
-                                    value: 15,
-                                    message: '최대 15자 이하를 입력해주세요'
-                                }
-                            })} />
+                        <div>
+                            <label>닉네임</label>
+                            <input className="inputField"
+                                placeholder="최소 2자~ 최대15자" {...register('nickname', {
+                                    required: '닉네임을 입력해주세요',
+                                    minLength: {
+                                        value: 2,
+                                        message: '최소 2자 이상을 입력해주세요'
+                                    },
+                                    maxLength: {
+                                        value: 15,
+                                        message: '최대 15자 이하를 입력해주세요'
+                                    }
+                                })} />
+                        </div>
                         {errors.nickname && <p>{errors.nickname.message}</p>}
                     </div>
 
                     <div className='password-wrap'>
-                        <label>비밀번호</label>
-                        <input type='password' id='password' className="inputField"
-                            placeholder="영문, 숫자, 특수문자 포함 8자 이상" {...register('password', {
-                                required: '비밀번호를 입력해주세요',
-                                minLength: {
-                                    value: 8,
-                                    message: '최소 8자 이상의 비밀번호를 입력해주세요'
-                                },
-                                pattern: {
-                                    value:
-                                      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
-                                    message:
-                                      '비밀번호는 숫자, 영문, 특수문자를 포함한 8글자 이상이어야 합니다.',
-                                  },
-                            })} />
+                        <div>
+                            <label>비밀번호</label>
+                            <input type='password' id='password' className="inputField"
+                                placeholder="영문, 숫자, 특수문자 포함 8자 이상" {...register('password', {
+                                    required: '비밀번호를 입력해주세요',
+                                    minLength: {
+                                        value: 8,
+                                        message: '최소 8자 이상의 비밀번호를 입력해주세요'
+                                    },
+                                    pattern: {
+                                        value:
+                                            /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
+                                        message:
+                                            '비밀번호는 숫자, 영문, 특수문자를 포함한 8글자 이상이어야 합니다.',
+                                    },
+                                })} />
+                        </div>
                         {errors.password && <p>{errors.password.message}</p>}
                     </div>
 
                     <div className='repassword-wrap'>
                         <label>비밀번호 확인</label>
-                        <input type='password' className="inputField" />
+                        <input type='password' className="inputField" ref={verifypw} />
                     </div>
 
                     <div className='phone-wrap'>
-                        <label >핸드폰 번호</label>
-                        <input className="inputField"
-                            placeholder="01012345678" {...register('phoneNumber', {
-                                required: '핸드폰번호를 입력해주세요'
-                            })} />
+                        <div>
+                            <label >핸드폰 번호</label>
+                            <input className="inputField"
+                                placeholder="01012345678" {...register('phoneNumber', {
+                                    required: '핸드폰번호를 입력해주세요'
+                                })} />
+                        </div>
                         {errors.phoneNumber && <p>{errors.phoneNumber.message}</p>}
                     </div>
 
                     <div className='address-wrap'>
                         <div>
-                        <label >주소</label>
-                        {/* 우편번호 zipcode (ex. 50139) */}
-                        <input className='inputZipcode' value={zipcode} onClick={() => { addresstoggle() }}></input>
-                        <button onClick={(e) => {
-                            addresstoggle();
-                            e.preventDefault();
-                        }}>주소찾기</button>
+                            <label >주소</label>
+                            {/* 우편번호 zipcode (ex. 50139) */}
+                            <input className='inputZipcode' value={zipcode} onClick={() => { addresstoggle() }}></input>
+                            <button onClick={(e) => {
+                                addresstoggle();
+                                e.preventDefault();
+                            }}>주소찾기</button>
                         </div>
                         <input className='inputAddress' value={address} onClick={() => { addresstoggle() }} ></input>
                     </div>
