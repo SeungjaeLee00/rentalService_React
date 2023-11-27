83bbbc1c8c108d58fa335d60d89388481af07cf3
choims <chlaudtn0257@gmail.com>
2023-11-27 09:42:56 +0900

    refactor:대쉬보드 clean up에서 clearInterval수행

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -4,6 +4,8 @@ import { useEffect } from 'react';
 import { useRef } from 'react';
 import arrowright from '../assets/img/arrow-right.png';
 import arrowleft from '../assets/img/arrpw-left.png'
+
+
 export default function Carousel({ carouselList }) {
     const slideRef = useRef(null);
     const [currentImgIdx, setCurrentImgIdx] = useState(0);
@@ -11,18 +13,23 @@ export default function Carousel({ carouselList }) {
     const slideRange = currentImgIdx * IMG_WIDTH;
 
     useEffect(() => {
-        slideRef.current.style.transition = "all 1s ease-in-out";
+        slideRef.current.style.transition = "all 2s ease-in-out";
         slideRef.current.style.transform = `translateX(-${slideRange}px)`;
         let time = setTimeout(()=>{
-            clearInterval(time);
             if (currentImgIdx == 1) setCurrentImgIdx(currentImgIdx -1);
-            if (currentImgIdx == 0) setCurrentImgIdx(currentImgIdx +1);
-        },4500)
+            else if (currentImgIdx == 0) setCurrentImgIdx(currentImgIdx +1);
+        },5000)
+        return()=>{
+            clearInterval(time);
+        }
     }, [currentImgIdx]);
 
     const moveToNextSlide = () => {
-        if (currentImgIdx == 1) return;
-        setCurrentImgIdx(currentImgIdx + 1);
+        if (currentImgIdx == 1)
+        {
+            return; 
+        }
+        else setCurrentImgIdx(currentImgIdx + 1);
         
     }
     const moveToPrevSlide = () => {
@@ -36,20 +43,21 @@ export default function Carousel({ carouselList }) {
             <LeftBtn onClick={moveToPrevSlide}></LeftBtn>
             <Wrapper>
                 <SlideWrapper ref={slideRef}>
-                    <Img >
+                    <Dash >
                         <div className='dashboard-right'>
                             <div className='dashboard-title'>
                                 <h1 style={{ fontWeight: "bold", fontSize: "50px", color:"blue" }}>Billim</h1>
                             </div>
                             <div style={{ marginTop: "20px" }} className='dashboard-decoration'>
                                 <div style={{ marginBottom: "50px", fontSize:"30px" }}>
-                                    <p>언제어디서든지 상품을</p> <br/><p><span style={{color:"blue",fontSize:"35px",fontWeight:"bold"}}>대여</span> 해주고 
-                                     <span style={{color:"blue",fontSize:"35px",fontWeight:"bold"}}> 대여</span> 받을 수 있는 서비스입니다.</p>
+                                    <p>언제어디서든지 상품을 쉽게</p><br/> <p><span style={{color:"blue",fontSize:"35px",fontWeight:"bold"}}>대여</span> 해주고 
+                                     <span style={{color:"blue",fontSize:"35px",fontWeight:"bold"}}> 대여</span> 받을 수 있는 서비스를 제공합니다.<br/>
+                                     </p>
                                     </div>
                             </div>
                         </div>
-                    </Img>
-                    <Img>
+                    </Dash>
+                    <Dash>
                         <div className='dashboard-right'>
                             <div className='dashboard-title'>
                                 <h1 style={{ fontWeight: "bold", fontSize: "50px",color:"blue" }}>Billim</h1>
@@ -58,7 +66,7 @@ export default function Carousel({ carouselList }) {
                                 <div style={{ marginBottom: "50px", fontSize:"30px" }}><p>Billim이 처음이시라면?</p><br/><a>서비스 이용방법 보러가기</a></div>
                             </div>
                         </div>
-                    </Img>
+                    </Dash>
                 </SlideWrapper>
             </Wrapper>
             <RightBtn onClick={moveToNextSlide}></RightBtn>
@@ -74,19 +82,16 @@ const Wrapper = styled.div`
 width:800px;
 height:200px;
 overflow:hidden;
-margin:0px 80px;
+margin:20px 80px;
 `;
 const SlideWrapper = styled.div`
 display:flex;
-width:100%;
-height:100%;
+width:700px;
+height:150px;
 `;
-const Img = styled.div`
+const Dash = styled.div`
 width:800px;
 height:200px;
-background-position: 50% 50%;  // 가운데로 보내고
-background-size: contain;	   // 아래의 코드랑 같이 쓰면 한개의 이미지가 나옴
-background-repeat: no-repeat;
 flex: none;	  				  // 이 속성을 넣어야 화면에 1개씩 보여진다.
 `;
 
